1818bc4d41cba04f07b649ef3281e16daa818b18
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-07-24 03:57:45 +0200

    Add sponsorship types

--- assets/js/init.js
+++ assets/js/init.js
@@ -4,5 +4,14 @@
         collapseButton.sideNav();
         $('.remove-sideNav').click(function () { collapseButton.sideNav('hide'); });
         $('.parallax').parallax();
+        
+        var onResize = function () {
+            var sponsorPackages = $(".details > ul");
+            var heights = sponsorPackages.map(function () { return $(this).height(); }).get();
+            var maxHeight = Math.max.apply(null, heights);
+            sponsorPackages.map(function () { $(this).parent().height(maxHeight + 150); });
+        };
+        $(window).resize(onResize);
+        onResize();
     });
 })(jQuery);
