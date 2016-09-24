e5e0593fdfee462644c8a3b56069e9ad64bdf650
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-09-24 18:20:50 +0200

    Add venue using Google Maps with API key

--- assets/js/init.js
+++ assets/js/init.js
@@ -12,6 +12,27 @@
             sponsorPackages.map(function () { $(this).parent().height(maxHeight + 150); });
         };
         $(window).resize(onResize);
+
+        google.maps.event.addDomListener(window, 'load', function () {
+            var mapOptions = {
+                zoom: 13,
+                scrollwheel: false,
+                panControl: false,
+                mapTypeControl: false,
+                streetViewControl: false,
+                center: new google.maps.LatLng(44.51, 11.32)
+            };
+
+            var map = new google.maps.Map(document.getElementById('canvas-map'),mapOptions);
+            var image = 'assets/img/position.png';
+            var myLatLng = new google.maps.LatLng(44.5220075, 11.346994);
+            var beachMarker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                icon: image
+            });
+        });
+
         onResize();
     });
 })(jQuery);
